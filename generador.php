f10dfd44fcebf5e23b1e938a66fed61c98b00321
Tienda Virtual Batan <tiendavirtualbatan@gmail.com>
2023-08-20 22:21:38 -0600

    Actualizar

--- generador.php
+++ generador.php
@@ -1,21 +1,21 @@
 <?php
-   // Establecer una conexión con la base de datos MySQL
-   $conexion = new mysqli('localhost', 'root', '', 'tienda');
-   
-   // Verificar si la conexión fue exitosa
-   if ($conexion->connect_error) {
-       die('Error de conexión: ' . $conexion->connect_error);
-   }
-   
-   // Consulta SQL para seleccionar todos los registros de la tabla "productos"
-   $sql = "SELECT * FROM productos";
-   
-   // Ejecutar la consulta y almacenar el resultado en la variable $result
-   $result = $conexion->query($sql);
-   
-   // Iniciar el buffer de salida para almacenar la salida generada por el script
-   ob_start();
-   ?>
+// Establecer una conexión con la base de datos MySQL
+$conexion = new mysqli('localhost', 'root', '', 'tienda');
+
+// Verificar si la conexión fue exitosa
+if ($conexion->connect_error) {
+    die('Error de conexión: ' . $conexion->connect_error);
+}
+
+// Consulta SQL para seleccionar todos los registros de la tabla "productos"
+$sql = "SELECT * FROM productos";
+
+// Ejecutar la consulta y almacenar el resultado en la variable $result
+$result = $conexion->query($sql);
+
+// Iniciar el buffer de salida para almacenar la salida generada por el script
+ob_start();
+?>
 <!DOCTYPE html>
 <html lang="es-es">
 
@@ -31,6 +31,13 @@
     <!-- Enlace al archivo de estilos CSS -->
     <link rel="stylesheet" href="sistema/css/index.css" />
     <title>Tienda Virtual Batan</title>
+    <style>
+        .video-container {
+            position: absolute;
+            top: -9999px;
+            left: -9999px;
+        }
+    </style>
 </head>
 
 <body>
@@ -63,96 +70,96 @@
     <!-- Aquí se mostrarán los productos dinámicamente mediante código JavaScript -->
     <div class="product-container">
         <?php
-            // Verifica si hay resultados en la consulta
-            if ($result->num_rows > 0) {
-                // Recorre cada fila de resultados
-                while ($row = $result->fetch_assoc()) {
-                    // Muestra un producto en un div con el atributo de data-categoria
-                    echo '<div class="product" data-categoria="' . $row['categoria'] . '">';
-            
-                    // Muestra la imagen del producto con un enlace para la expansión
-                    echo '<div class="product">';
-                    echo '<img src="' . $row['imagen'] . '" alt="' . $row['nombre'] . '" class="expandable-image">';
-                    echo '</div>';
-            
-                    // Agrega el enlace "Más detalles" que mostrará el contenido adicional al hacer clic en él
-                    echo '<a href="#" class="show-details">Más detalles</a>';
-            
-                    // Muestra los detalles del producto en un div con la clase "product-details" (inicialmente oculto)
-                    echo '<div class="product-details" style="display:none;">';
-                    
-                    // Muestra el nombre del producto
-                    echo '<h3>' . $row['nombre'] . '</h3>';
-            
-                    // Muestra el precio del producto formateado como moneda (colones, con dos decimales)
-                    echo '<p>₡' . number_format($row['precio'], 2) . '</p>';
-            
-                    // Muestra la categoría del producto
-                    echo '<span>Categoría: ' . $row['categoria'] . '</span>';
-            
-                    // Muestra el tipo de envío del producto
-                    echo '<span>Envío en: ' . $row['envio'] . ' días</span>';
-            
-                    // Muestra la cantidad disponible del producto
-                    echo '<span>Disponibles: ' . $row['cantidad'] . ' unidades</span>';
-            
-                    // Genera un enlace de WhatsApp usando el número de teléfono del vendedor
-                    $whatsappLink = 'https://wa.me/506' . $row['telefono'];
-                    echo '<span>WhatsApp: <a href="' . $whatsappLink . '">' . $row['telefono'] . '</a></span>';
-            
-                    // Muestra el nombre de la tienda del producto
-                    echo '<span>Tienda: <span class="tienda">' . $row['tienda'] . '</span></span>';
-                    
-                    // Cierra el div de detalles del producto
-                    echo '</div>';
-            
-                    // Cierra el div del producto actual
-                    echo '</div>';
-                }
-            } else {
-                // Si no hay productos, muestra un mensaje indicando que no hay registros.
-                echo "<p>No hay productos registrados.</p>";
+        // Verifica si hay resultados en la consulta
+        if ($result->num_rows > 0) {
+            // Recorre cada fila de resultados
+            while ($row = $result->fetch_assoc()) {
+                // Muestra un producto en un div con el atributo de data-categoria
+                echo '<div class="product" data-categoria="' . $row['categoria'] . '">';
+
+                // Muestra la imagen del producto con un enlace para la expansión
+                echo '<div class="product">';
+                echo '<img src="' . $row['imagen'] . '" alt="' . $row['nombre'] . '" class="expandable-image">';
+                echo '</div>';
+
+                // Agrega el enlace "Más detalles" que mostrará el contenido adicional al hacer clic en él
+                echo '<a href="#" class="show-details">Más detalles</a>';
+
+                // Muestra los detalles del producto en un div con la clase "product-details" (inicialmente oculto)
+                echo '<div class="product-details" style="display:none;">';
+
+                // Muestra el nombre del producto
+                echo '<h3>' . $row['nombre'] . '</h3>';
+
+                // Muestra el precio del producto formateado como moneda (colones, con dos decimales)
+                echo '<p>₡' . number_format($row['precio'], 2) . '</p>';
+
+                // Muestra la categoría del producto
+                echo '<span>Categoría: ' . $row['categoria'] . '</span>';
+
+                // Muestra el tipo de envío del producto
+                echo '<span>Envío en: ' . $row['envio'] . ' días</span>';
+
+                // Muestra la cantidad disponible del producto
+                echo '<span>Disponibles: ' . $row['cantidad'] . ' unidades</span>';
+
+                // Genera un enlace de WhatsApp usando el número de teléfono del vendedor
+                $whatsappLink = 'https://wa.me/506' . $row['telefono'];
+                echo '<span>WhatsApp: <a href="' . $whatsappLink . '">' . $row['telefono'] . '</a></span>';
+
+                // Muestra el nombre de la tienda del producto
+                echo '<span>Tienda: <span class="tienda">' . $row['tienda'] . '</span></span>';
+
+                // Cierra el div de detalles del producto
+                echo '</div>';
+
+                // Cierra el div del producto actual
+                echo '</div>';
             }
-            
-            // Cierra la conexión a la base de datos
-            $conexion->close();
-            ?>
+        } else {
+            // Si no hay productos, muestra un mensaje indicando que no hay registros.
+            echo "<p>No hay productos registrados.</p>";
+        }
+
+        // Cierra la conexión a la base de datos
+        $conexion->close();
+        ?>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script>
-        $(document).ready(function() {
-            $(".show-details").click(function(event) {
-                event.preventDefault(); // Evita la acción predeterminada del enlace
+            $(document).ready(function() {
+                $(".show-details").click(function(event) {
+                    event.preventDefault(); // Evita la acción predeterminada del enlace
 
-                var productDetails = $(this).next(".product-details");
+                    var productDetails = $(this).next(".product-details");
 
-                // Encuentra todos los detalles del producto, excepto el que se está abriendo
-                var otherDetails = $(".product-details").not(productDetails);
+                    // Encuentra todos los detalles del producto, excepto el que se está abriendo
+                    var otherDetails = $(".product-details").not(productDetails);
 
-                // Si el detalle actual está visible, ocúltalo; si no, muestra y oculta los demás detalles
-                if (productDetails.is(":visible")) {
-                    productDetails.hide();
-                } else {
-                    otherDetails.hide();
-                    productDetails.show();
-                }
-            });
-
-            // Cierra los detalles de los productos cuando se hace clic fuera de ellos
-            $(document).on("click", function(event) {
-                if (!$(event.target).closest(".show-details, .product-details").length) {
-                    $(".product-details").hide();
-                }
-            });
-
-        });
-
-        // Agrega esta sección al final de tu archivo HTML, dentro de la etiqueta <body> 
-
-        $(document).ready(function() {
-                $(".expandable-image").click(function() {
-                            $(this).toggleClass("expanded");
+                    // Si el detalle actual está visible, ocúltalo; si no, muestra y oculta los demás detalles
+                    if (productDetails.is(":visible")) {
+                        productDetails.hide();
+                    } else {
+                        otherDetails.hide();
+                        productDetails.show();
+                    }
                 });
-        });
+
+                // Cierra los detalles de los productos cuando se hace clic fuera de ellos
+                $(document).on("click", function(event) {
+                    if (!$(event.target).closest(".show-details, .product-details").length) {
+                        $(".product-details").hide();
+                    }
+                });
+
+            });
+
+            // Agrega esta sección al final de tu archivo HTML, dentro de la etiqueta <body> 
+
+            $(document).ready(function() {
+                $(".expandable-image").click(function() {
+                    $(this).toggleClass("expanded");
+                });
+            });
         </script>
 
     </div>
@@ -160,46 +167,41 @@
     <div class="sticky-container">
         <ul class="sticky">
             <li>
-                <p><a href="https://wa.me/50687895592" target="_self"><img src="sistema/imagenes/whatsapp.png"
-                            width="35" height="35"></a></p>
+                <p><a href="https://wa.me/50687895592" target="_self"><img src="sistema/imagenes/whatsapp.png" width="35" height="35"></a></p>
             </li>
             <li>
-                <p><a href="https://www.youtube.com/channel/UCQla-SX_xSInWU86Z4z0iYg" target="_self"><img
-                            src="sistema/imagenes/youtube.png" width="35" height="35"></a></p>
+                <p><a href="https://www.youtube.com/channel/UCQla-SX_xSInWU86Z4z0iYg" target="_self"><img src="sistema/imagenes/youtube.png" width="35" height="35"></a></p>
             </li>
             <li>
-                <p><a href="https://www.instagram.com/yuliana.emir/" target="_self"><img
-                            src="sistema/imagenes/instagram.png" width="35" height="35"></a></p>
+                <p><a href="https://www.instagram.com/yuliana.emir/" target="_self"><img src="sistema/imagenes/instagram.png" width="35" height="35"></a></p>
             </li>
             <li>
-                <p><a href="https://www.facebook.com/tiendavirtualbataan/" target="_self"><img
-                            src="sistema/imagenes/facebook.png" width="35" height="35"></a></p>
+                <p><a href="https://www.facebook.com/tiendavirtualbataan/" target="_self"><img src="sistema/imagenes/facebook.png" width="35" height="35"></a></p>
             </li>
             <li>
-                <p><a href="https://twitter.com/emiryuliana" target="_self"><img src="sistema/imagenes/twiter.gif"
-                            width="35" height="35"></a></p>
+                <p><a href="https://twitter.com/emiryuliana" target="_self"><img src="sistema/imagenes/twiter.gif" width="35" height="35"></a></p>
             </li>
             <li>
-                <p><a href="emiryuliana/index.html" target="_self"><img src="sistema/imagenes/logo.png" width="35"
-                            height="35"></a></p>
+                <p><a href="emiryuliana/index.html" target="_self"><img src="sistema/imagenes/logo.png" width="35" height="35"></a></p>
             </li>
         </ul>
     </div>
     <!-- Archivo JavaScript para manipulación de la página -->
     <script src="sistema/js/index.js"></script>
+    <iframe class="video-container" width="560" height="315" src="https://www.youtube.com/embed/_OE4EqC5yOQ?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </body>
 
 </html>
 <?php
-   // Finalizar almacenamiento en buffer y guardar el contenido en una variable
-   $htmlContent = ob_get_clean();
-   
-   // Guardar el contenido del buffer en un archivo HTML llamado index.html
-   file_put_contents('index.html', $htmlContent);
-   
-   // Redirigir al usuario a index.html
-   header('Location: index.html');
-   
-   // Asegurar que no se ejecute más código después de la redirección
-   exit();
-   ?>
+// Finalizar almacenamiento en buffer y guardar el contenido en una variable
+$htmlContent = ob_get_clean();
+
+// Guardar el contenido del buffer en un archivo HTML llamado index.html
+file_put_contents('index.html', $htmlContent);
+
+// Redirigir al usuario a index.html
+header('Location: index.html');
+
+// Asegurar que no se ejecute más código después de la redirección
+exit();
+?>
