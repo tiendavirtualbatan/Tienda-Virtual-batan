2e77d1e37e9f47c6f384b9079bafe57db85459c4
Tienda Virtual Batan <tiendavirtualbatan@gmail.com>
2023-08-26 03:52:58 -0600

    Actualizar

--- Paginas/guardar-contactos/generar-html-contactos.php
+++ Paginas/guardar-contactos/generar-html-contactos.php
@@ -10,92 +10,19 @@ if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 
-$output = '<!DOCTYPE html>
+$output = '
+<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-        }
-
-        .container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 20px;
-        }
-
-        .category-buttons {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 20px;
-            flex-wrap: wrap;
-        }
-
-        .category-button {
-            margin: 10px;
-            padding: 5px 10px;
-            background-color: #3498db;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        .contact-details-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-        }
-
-        .contact-details {
-            display: none;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 10px;
-            background-color: #eaeaea;
-            border-radius: 5px;
-            margin: 10px;
-            width: 120px;
-            height: auto;
-        }
-
-        .contact-details h3,
-        .contact-details p {
-            margin: 5px;
-        }
-
-        .contact-img {
-            width: 100px;
-            height: 100px;
-            object-fit: cover;
-            border-radius: 50%;
-        }
-
-        .back-link {
-            text-decoration: none; /* Quitar subrayado */
-            color: red; /* Cambiar color a rojo */
-            font-weight: bold; /* Hacer el texto en negrita */
-        }
-
-        .back-link:hover {
-            text-decoration: underline; /* Agregar subrayado al pasar el mouse */
-        }
-
-    </style>
+    <link rel="stylesheet" href="index.css" />
     <title>Ver Categorías</title>
 </head>
 <body>
     <div class="container">
-    <h1><a class="back-link" href="../../index.html">Regresar a Tienda</a></h1>
-    <div class="category-buttons">';
+        <h1><a class="back-link" href="../../index.html">Regresar a Tienda</a></h1>
+        <div class="category-buttons">';
 
 $categoria_query = "SELECT * FROM categorias";
 $categoria_result = $conn->query($categoria_query);
@@ -121,10 +48,10 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $output .= '<div class="contact-details" data-categoria="' . $row["categoria_nombre"] . '">';
         $output .= '<img class="contact-img" src="uploads/' . $row["img"] . '" alt="' . $row["nombre"] . '">';
-        $output .= '<h3>' . $row["nombre"] . '</h3>';
-        $output .= '<p><strong>Llamada:</strong><br>&nbsp;<a href="tel:' . $row["numeros"] . '" style="color: red; text-decoration: none; font-weight: bold;">' . $row["numeros"] . '</a></p>';
-        $output .= '<p>&nbsp;<a href="https://wa.me/506' . $row["numeros"] . '" style="color: green; text-decoration: none; font-weight: bold;">WhatsApp</a></p>';
-        $output .= '<p><strong>Categoría:</strong><br>&nbsp;' . $row["categoria_nombre"] . '</p>';
+        $output .= '<h3 class="contact-name">' . $row["nombre"] . '</h3>';
+        $output .= '<p class="contact-link llamada"><strong class="contact-strong">Llamada:</strong><br>&nbsp;<a href="tel:' . $row["numeros"] . '">' . $row["numeros"] . '</a></p>';
+        $output .= '<p class="contact-link whatsapp"><a href="https://wa.me/506' . $row["numeros"] . '">WhatsApp</a></p>';
+        $output .= '<p class="contact-category"><strong>Categoría:</strong><br>&nbsp;' . $row["categoria_nombre"] . '</p>';
         $output .= '</div>';
     }
 } else {
